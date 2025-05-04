6b4b8921d55386ff6cc60785b3f457d36898a90c
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-04 11:34:06 +0500

    now the day first and without installation through cdn link import  html

---
+++ First Video/app.js
@@ -0,0 +1,8 @@
+console.log("Hello From App.js");
+
+const app = Vue.createApp({
+  // your options, data, methods, etc.
+  template: "<h1>Vue.js Tutorial In app Comp</h1>"
+});
+
+app.mount("#app");
