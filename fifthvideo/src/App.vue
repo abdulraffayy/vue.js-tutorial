3423f87d553f5fb5c1795109bb9f21ba78e58644
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-04 17:08:14 +0500

    Interpolation and Data and we can use it function on script and use in a  template method

---
+++ fifthvideo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg=" Interpolation and Data"/>
+<HomePage/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomePage from "./components/Home.vue"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomePage,
+  }
+}
+</script>
+
---
+++ fifthvideo/src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1>Home Component Render {{ 10 + 10 }}</h1>
+    <h1>Home Component Render {{"HelloRafay".length}}</h1>
+    <h3>Email: {{email}}</h3>
+    <h3>mobileNumber: {{mobileNumber}}</h3>
+    <h5>Name:{{ getName("Hmaza")}}</h5>
+    <h6>Name:{{mobileNumber == 8888}}</h6>
+
+  
+
+
+
+
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+    data()
+    {
+        return {
+            email: "Anil@test.com",
+            mobileNumber: "12334354564656776787897979",
+            getName:function(a)
+            {
+                return a
+            }
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+h1{
+    color: red
+}
+
+h3 {
+    color: black;
+    background-color: rgb(66, 105, 14);
+}
+
+h5 {
+    color: rgb(165, 51, 51);
+    background-color: gray;
+}
+</style>
