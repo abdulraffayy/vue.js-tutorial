bc86279138650b5abcfa09fe5576899177c53643
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-04 13:10:58 +0500

    now we can create a folder and specify the chnages and understand the code and chnages on app.vue and attached on Hello  world file

--- secondvideo/src/App.vue
+++ secondvideo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Step By Step"/>
+  <HelloWorld msg="Welcome to Step By Step" />
 </template>
 
 <script>
---
+++ thirdvideo/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<HelloWorld msg="Welcome to Code Step By Step" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+}
+</script>
