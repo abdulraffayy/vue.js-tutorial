5962755dea5b03ea97975cbdd243e3c9f6061dcb
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-04 22:09:07 +0500

    Methods with Params andd created a function and object will created and use it

---
+++ sixvideo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="Methods with Params"/>
+  <HomeData/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeData from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeData
+  }
+}
+</script>
+
---
+++ sixvideo/src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <h2>Methods Wit Params Home Comp</h2>
+    <h2>Email Copy: {{email}}</h2>
+    <h3>Name: {{getName("Peter")}}</h3>
+    <h3>All Data: {{getData()}}</h3>
+    <h3>All Data Name: {{getData().name}}</h3>
+    <h3>All Data: {{getData().email}}</h3>
+
+
+
+
+
+</template>
+
+<script>
+export default{
+    name: "HomeData",
+    data() {
+        return {
+            email: "abdullahDevopscloudquik@gmail.com",
+        }
+    },
+    methods:{
+        getName(name)
+        {
+            return name;
+        }, 
+        getData () {
+            return {
+                name: "Rafay",
+                email: this.email
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+h2 {
+    color: cadetblue;
+}
+h3 {
+    color: blue;
+}
+</style>
