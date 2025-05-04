432586413326bc04719d70d2900137d6c094edfe
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-04 23:32:57 +0500

    Vue js Two Ways Binding

---
+++ eightvideo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<HelloWorld msg="Two way binding"/>
+<HomeThing/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeThing from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeThing
+  }
+}
+</script>
+
+
+<style scoped>
+
+h1 {
+  color: rebeccapurple
+}
+</style>
+
+
---
+++ eightvideo/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello Home COMPONENT </h1>
+    <input type="text" v-model="count"/>
+    <h2>Count: {{count}}</h2>
+</template>
+
+<script>
+export default {
+    name: "HomeThing",
+    data () {
+        return {
+            count:0
+        }
+    }
+}
+</script>
