6f935a8582a07d0c94807d132b622e139272ff05
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-04 22:40:54 +0500

    Event with example, click event Single click double click on Mouse Event simple counter data we will click functionalty we apply

---
+++ sevenvideo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ 
+  <HelloWorld msg="Event with example, click event"/>
+  <HomeData/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeData from "./components/Home.vue"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeData
+  }
+}
+</script>
+
---
+++ sevenvideo/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1 v-on:mousemove="MouseEvent">Home Page CliCK Event Paractice</h1>
+    <button v-on:click="getdata('Button 1 Clicked')">Click Me</button>
+    <button v-on:dblclick="getdata('Button double Clicked')">Click Me</button>
+    <h2>Count data:{{ data }} </h2>
+
+</template>
+
+<script>
+export default {
+    name:  "HomeData",
+    data() {
+   return {
+    data:2,
+   }
+    },
+    methods: {
+        getdata()
+        {
+            this.data = this.data+1;
+            
+        },
+        MouseEvent () {
+            console.log("Mouse Event Called")
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: cadetblue;
+}
+</style>
