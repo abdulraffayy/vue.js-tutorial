a6638ca73b3c615cacab78e7229209b32723e2e3
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 11:26:26 +0500

    if else conditions | conditional rendering and toogle button interview question solve

---
+++ elevenvideo/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>if else conditions | conditional rendering</h2>
+    <h2 v-if="show">If Condition</h2>
+    <h2 v-else>Else Condition</h2>
+
+
+    <button v-on:click="show=!show">Toggle</button>
+
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+    data() {
+        return {
+            show:false
+        }
+    }
+}
+</script>
+
+<style>
+h2 {
+    color: blue;
+}
+</style>
