38c7a89bc41c5e75f196d86d5328fea29509025f
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 16:56:51 +0500

    Ref with Interview question an d we can chnage fouc and so on

---
+++ twentyvideo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeRafayRaja />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeRafayRaja from './components/Home.vue' 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeRafayRaja,
+  }
+}
+</script>
+
+
---
+++ twentyvideo/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Ref with Interview question</h1>
+    <input type="text" ref="inputRef" />
+    <button  v-on:click="getData">Clicked Data</button>
+</template>
+
+<script>
+export default {
+    name: 'HomeRafayRaja',
+    methods: {
+        getData(){
+            this.$refs.inputRef.focus()
+            this.$refs.inputRef.style.color = 'red'
+            
+            let value = this.$refs.inputRef.value
+            console.log(value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
