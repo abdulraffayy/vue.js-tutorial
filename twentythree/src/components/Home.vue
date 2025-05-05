3b3f43c795c50e95ce316a7421986b006e65faa4
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 21:42:00 +0500

    modifer

---
+++ twentythree/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Home Comp</h1>
+    <input type="text" v-model.trim="data" />
+    <h2>Data: {{ data }}</h2>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            data: ''
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+h1 {
+    color: red;
+}
+
+</style>
