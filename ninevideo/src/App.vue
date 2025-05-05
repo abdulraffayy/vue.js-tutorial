55c354d88fed349813f1a2d72b50c0315498494e
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 10:12:26 +0500

    Get input value and wwe can use the bind with model turoial

---
+++ ninevideo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HelloWorld msg="Get input value" />
+  <input v-model="email" type="text" placeholder="Enter Email" />
+  <br><br>
+  <input v-model="password" type="password" placeholder="Enter Password" />
+  <br><br>
+  <button v-on:click="getData" type="button">Get Value</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      email: null,
+      password: null,
+    }
+  },
+  methods: {
+    getData() {
+      console.log("Value:  ",this.email ,this.password);
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
