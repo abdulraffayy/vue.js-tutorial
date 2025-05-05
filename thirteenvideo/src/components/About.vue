fdf6e1d95303ce3cc6d621db11125c0f25e5baa3
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 12:56:02 +0500

    Pass Data Parent to Child Component from parent to child data send to paent to child on About.vue to Child.vue

---
+++ thirteenvideo/src/components/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <h2>Pass Data Parent to Child Component</h2>
+    <ChildDataComponent name="Anil Sidu" :users="users" :getData="getData"/>
+</template>
+
+<script>
+import ChildDataComponent from './Child.vue';
+
+export default {
+    name: "AboutPage",
+    components: {
+        ChildDataComponent,
+    },
+    data() {
+
+        return {
+            users: {name: "Peter", email: "Peter43@gmail.com"},
+        }
+    },
+    methods:{
+        getData() {
+            console.log("Parent Function CALLED")
+        }
+    }
+}
+</script>
+
+<style>
+h2 {
+    color: chartreuse;
+}
+</style>
---
+++ thirteenvideo/src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>Child Comnponent  {{name}}</h3>
+    <h3>User Data Will Show  {{users.name}}</h3>
+    <button v-on:click="getData()">Called Fucntion</button>
+ 
+
+
+
+</template>
+
+<script>
+export default {
+    name: "ChildDataComponent", 
+    props: {
+        name:String,
+        users:Object,
+        getData:Function,
+    }
+}
+</script>
+
+<style>
+h3 {
+    color: chocolate;
+}
+</style>
