0131f31b7c6c2fcc3d1e9423f87eeb773c00fd38
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 12:12:00 +0500

    For Loop and List creating array of objects

---
+++ twelvevideo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AboutSection/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AboutSection from './components/About.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    AboutSection
+  }
+}
+</script>
+
+
---
+++ twelvevideo/src/components/About.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1> For Loop and List</h1>
+    <ul>
+    <li v-for="rafay in technology" :key="rafay">
+    {{rafay}}
+    </li>
+    </ul>
+    <br/>
+
+    <ul>
+    <li v-for="item in users" :key="item">
+   Name is: {{item.name}} and Email is: {{item.email }}
+    
+    </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'AboutSection',
+    data() {
+        return{
+            technology: ['java', 'php', 'Node', 'HTML'],
+            users:[
+            {name:"Rafay", email: "rafayazad23@gmail.com"},
+            {
+                name:"Ali Sheir", 
+                email: "Alisheirshah23@gmail.com"
+
+
+            },
+            {
+                name:"Bruce", 
+                email: "Bruce23@gmail.com"
+
+
+            }
+
+        ]
+        }
+      
+    }
+}
+</script>
