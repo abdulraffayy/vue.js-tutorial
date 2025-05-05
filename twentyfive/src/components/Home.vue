1123750f4bea8ae9e9f390ba5cdeb3adb20c27a0
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 23:27:08 +0500

    Watchers with example an event with calling some action look like this

---
+++ twentyfive/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Computed Property</h1>  
+    <h2>Counter: {{ counter }}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    watch: {
+      counter(value, previousValue) {
+        if(value > 5 && value>previousValue) {
+            alert("Stopping the counter");
+        }
+
+      }
+    }
+
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: red;
+  }
+  </style>
+  
