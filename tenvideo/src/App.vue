5ce763ecfbadd056dfdf2e78cccdb189a6ea9147
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 10:43:46 +0500

    Get Checkbox & Radio Button Value now we can created a input and v-model through get input and radio button

---
+++ tenvideo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeData/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeData from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeData,
+  }
+}
+</script>
---
+++ tenvideo/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+    <h2>Get Checkbox and  Radio Button Value</h2>
+    <h3>Technology</h3>
+    <label for="java">Java</label>
+
+    <input type="checkbox" value="java" id="java" v-model="technology"/>
+    <label for="php">PHP</label>
+    <input type="checkbox" value="php" id="php" v-model="technology"/>
+
+    <label for="node">Node</label>
+<input type="checkbox" value="node" id="node" v-model="technology"/>
+
+<br/><br/>
+<h3>Who i AM</h3>
+<label for="student">Node</label>
+<input type="radio" value="student" id="student" name="who" v-model="who" />
+<br/><br/>
+
+<label for="developer">developer</label>
+<input type="radio" value="developer" id="developer" name="who" v-model="who" />
+<h4>Selected Technology: {{technology}}</h4>
+<h4>Who I am: {{who}}</h4>
+
+</template>
+
+<script>
+export default {
+    name: "HomeData",
+    data () {
+        return {
+            technology:[],
+            who: null
+        }
+    }
+}
+</script>
+
+<style>
+h2 {
+    color: aquamarine;
+    font-size: 50px;
+
+}
+</style>
