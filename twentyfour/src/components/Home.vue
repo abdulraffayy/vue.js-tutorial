8c5d496f85020dc543d955ddc798f2bfe0145c90
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-05 22:02:05 +0500

    Non props data and props data

---
+++ twentyfour/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Non-Props Data</h1>
+    <UserPage data="John Doe" id="user-component" />
+</template>
+
+<script>
+import UserPage from './User.vue'
+export default {
+    name: 'HomePage',
+    components: {
+        UserPage
+    }           
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>                            
---
+++ twentyfour/src/components/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>User</h1>
+    <div >
+        <h3>Peter</h3>
+        <h3>JohN</h3>
+        <h3>bRUce</h3>
+        <h3>Data: {{ data }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage',
+    props: {
+        data: String
+    }
+}                       
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
