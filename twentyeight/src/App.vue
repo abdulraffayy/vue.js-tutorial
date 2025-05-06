1e16effd67c46c77f0a25b6c91483bf016faae68
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-06 10:40:46 +0500

    teleport concept we can reach in vue js 3

---
+++ twentyeight/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <teleport to="#footer">
+    <FooterComponent />
+  </teleport>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FooterComponent from './components/Footer.vue' 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FooterComponent
+  }
+}
+</script>
+
+
---
+++ twentyeight/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer">
+        <h1>Footer</h1>
+        <p>Copyright 2025</p>
+        <p>All rights reserved</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
+
+<style scoped>
+.footer {
+    background-color:rgb(14, 238, 141);
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    padding: 20px;
+}
+</style>
+
+
