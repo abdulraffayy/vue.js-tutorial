7b9a760695d6a480443b1e1e5a43ff6b3a0c6e6b
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-06 11:40:02 +0500

    before updated and updaed

---
+++ thirtyonevideo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeComponentProps />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeComponentProps from './components/Hom.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeComponentProps
+  }
+}
+</script>
+
---
+++ thirtyonevideo/src/components/Hom.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Home</h1>
+    <h1>Counter: {{ count }}</h1>
+    <button v-on:click="count=count+1">Updated Now</button>
+
+</template>
+
+<script>
+export default {
+    name: 'HomeComponentProps',
+    data() {
+        return {
+            count: 0
+        }
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate', this.$refs.count)
+    },
+    updated() {
+        console.log('updated', this.$refs.count)
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: rgba(79, 222, 13, 0.298);
+}
+</style>
